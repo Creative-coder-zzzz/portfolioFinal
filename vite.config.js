9bf4c4df0c5233df794945eed64d1a76350579a2
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2024-07-06 16:08:58 +0530

    Final ig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ['react-intersection-observer'], // 
+    },
+  },
+  resolve: {
+    alias: {
+      'framer-motion': 'framer-motion',
+      'react-intersection-observer': 'react-intersection-observer', 
+    },
+  },
+});
