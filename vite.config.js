5ab10a5a8b96413ad5157bdba7bab927aff383b9
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2024-07-06 15:47:14 +0530

    Last

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import ts from '@vitejs/plugin-typescript';
-import postcss from 'vite-plugin-postcss';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ts(),
-    postcss({
-      // PostCSS plugin options
-    }),
-  ],
-});
+  plugins: [react()],
+})
