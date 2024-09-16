359e4895c5a2291f45874f881ff40ffde09c5a2f
Sandesh Adhikari <sandeshadhikari2003@gmail.com>
2024-09-16 23:27:06 +0530

    parallax layer removed

--- src/App.jsx
+++ src/App.jsx
@@ -42,73 +42,57 @@ function App() {
   };
   return (
     <>
-      <Parallax pages={5}>
-        <ParallaxLayer
-          offset={0}
-          speed={0.5}
-          factor={7}
-          className="bg-gradient-to-r from-purple-500 to-violet-500"
-        >
-          <section
-            className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Wireframe.png')] z-10"
-            ref={arrow}
-          >
-            <Header
-              about={handleabout}
-              project={handleproject}
-              skill={handleskill}
-              contact={handlecontact}
-            />
+      <section
+        className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Wireframe.png')] z-10"
+        ref={arrow}
+      >
+        <Header
+          about={handleabout}
+          project={handleproject}
+          skill={handleskill}
+          contact={handlecontact}
+        />
 
-            <Name />
+        <Name />
 
-            <SocialMedia />
-          </section>
-        </ParallaxLayer>
+        <SocialMedia />
+      </section>
 
-        <ParallaxLayer offset={1} speed={1}>
-          <section
-            className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/About.png')] "
-            ref={ref}
-          >
-            <About />
-          </section>
-        </ParallaxLayer>
+      <section
+        className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/About.png')] "
+        ref={ref}
+      >
+        <About />
+      </section>
 
-        <ParallaxLayer offset={3} speed={0.8}>
-          <section
-            className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Projects.png')] "
-            ref={project}
-          >
-            <div className="flex justify-center mt-">
-              <h1 className="text-white text-3xl font-k2d font-semibold mt-5">
-                MY PROJECTS
-              </h1>
-            </div>
-            <CardSlider cards={cardData} />
-          </section>
-        </ParallaxLayer>
+      <section
+        className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Projects.png')] "
+        ref={project}
+      >
+        <div className="flex justify-center mt-">
+          <h1 className="text-white text-3xl font-k2d font-semibold mt-5">
+            MY PROJECTS
+          </h1>
+        </div>
+        <CardSlider cards={cardData} />
+      </section>
 
-        <ParallaxLayer offset={2} speed={0.8}>
-          <section
-            className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Skills.png')] "
-            ref={skill}
-          >
-            <div className="flex justify-center mt-"></div>
-            <Skills />
-          </section>
-        </ParallaxLayer>
+      <section
+        className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Skills.png')] "
+        ref={skill}
+      >
+        <div className="flex justify-center mt-"></div>
+        <Skills />
+      </section>
 
-        <ParallaxLayer offset={4} speed={0.8}>
-          <section
-            className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Contact.png')] "
-            ref={contact}
-          >
-            <div className="flex justify-center mt-"></div>
-            <Contact />
-          </section>
-        </ParallaxLayer>
-      </Parallax>
+      <section
+        className=" w-full h-screen overflow-hidden bg-cover bg-center bg-no-repeat bg-[url('./assets/images/Contact.png')] "
+        ref={contact}
+      >
+        <div className="flex justify-center mt-"></div>
+        <Contact />
+      </section>
+
       <div>
         <UpArrow arrow={handlearrow} className="z-0" />
       </div>
